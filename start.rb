ae1fc64aaeee974e1a4dd17b59f961de1a448cdb
Ethan <erdesi90562@gmail.com>
2016-05-19 13:51:13 -0400

    end of school

--- start.rb
+++ start.rb
@@ -1,27 +1,27 @@
 class Gameboard
-  def initalize(gameboard_size)
+  def initialize(gameboard_size)
     @gameboard_size = gameboard_size
   end
 
-  def game_box(arr)
-    arr.each do |index|
-      index.Hash.new { |hash, key| [key] = "kittens" }
-    end
+  def create
+    (0..@gameboard_size).each_with_object([]) { |index, arr| arr[index] = Box.new }
   end
-
-  def game_logic
-  end
+  #
+  # def game_box(arr)
+  # end
+  #
+  # def game_logic
+  # end
 end
 
 class Box
-  def initalize
+  def initialize
     @live = rand(2)
     @row = @row + 1
     @col = @col + 1
   end
 end
 
-
+# (0..@gameboard_size).each_with_object([]) { |index, arr| arr[index] = "fucking kitttens dude" }
 game = Gameboard.new(20)
-
 puts game
